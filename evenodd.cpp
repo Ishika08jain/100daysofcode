2710cd3c013a3d087c42c8d18d0f80fe66e35f47
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-05-15 09:46:04 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+    int i=1;
+    cin>>n;
+    int sum =0;
+    
+    while(i<=n){
+        if(i%2==0){
+            i=i+1;
+            sum = sum+2;
+            
+        }
+       
+    
+    }
+  
+}
