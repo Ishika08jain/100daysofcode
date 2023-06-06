37c3e63521ce70f0560248575d101a165199d950
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-06 20:42:03 +0530

    Add files via upload

---
+++ digitsofanum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     int nu;
+//     cin>>nu;
+//     while(nu!=0){
+//         int rem = nu%10;
+//         cout<<rem<<" "<<endl;
+//         nu=nu/10;
+//     }
+//     cout<<endl;
+// }
+
+
+int main(){
+    int n;
+    cout<<"Enter a number to be reversed: "<<endl;
+    cin>>n;
+    while(n!=0){
+        n = n*10;
+        
+    }
+      
+
+}
+
