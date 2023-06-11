e85a96c0546e27009f391d0d8184b487daf55504
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-11 20:31:17 +0530

    Add files via upload

---
+++ occur.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[] = {}
+    int n = arr.size();
+    int s =0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+    while(s<=e){
+        if(s==e){
+            return s;
+        }
+    }
+}
