eef6eb98c9b81a05064dd99a1a70b0d1daa9705d
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-14 21:39:24 +0530

    Add files via upload

---
+++ postordertraversal.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this-> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+
+}; 
+Node* buildTree(){
+    int data;
+    cout<< "Enter the data" << endl;
+    cin>> data;
+
+    if(data == -1){
+        return NULL;
+    }
+    Node* root = new Node(data);
+    cout << "Enter data for left part " << endl;
+
+    root -> left = buildTree();
+    cout << "Enter data for right part " << endl;
+
+    root -> right = buildTree();
+    return root;
+}
+
+void lOt(Node* root){
+    queue <Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp == NULL){
+            cout << endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+        cout << temp-> data <<" "; 
+        if(temp-> left){
+            q.push(temp-> left);
+
+        }
+        if(temp-> right){
+            q.push(temp -> right);
+        }
+    }
+    }
+
+
+
+
+}
+void postOrder(Node* root){
+    if(root == NULL){
+        return; 
+
+    }
+   
+    postOrder(root-> left);
+    
+    postOrder(root-> right);
+     cout << root-> data << " ";
+}
+int main(){
+    Node* root;
+    root = buildTree();
+    lOt(root);
+     
+    return 0;
+
+}
