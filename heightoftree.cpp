a1ab0c08af0115c9f3bc693eb684008e3fb7c0ae
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-22 19:55:59 +0530

    Add files via upload

---
+++ heightoftree.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this-> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+
+}; 
+Node* buildTree(){
+    int data;
+    cout<< "Enter the data" << endl;
+    cin>> data;
+
+    if(data == -1){
+        return NULL;
+    }
+    Node* root = new Node(data);
+    cout << "Enter data for left part " << endl;
+
+    root -> left = buildTree();
+    cout << "Enter data for right part " << endl;
+
+    root -> right = buildTree();
+    return root;
+}
+
+void lOt(Node* root){
+    queue <Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp == NULL){
+            cout << endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+        cout << temp-> data <<" "; 
+        if(temp-> left){
+            q.push(temp-> left);
+
+        }
+        if(temp-> right){
+            q.push(temp -> right);
+        }
+    }
+    }
+
+
+
+}
+
+
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftHeight = height(root-> left);
+    int rightHeight = height(root -> right);
+    int ans = max(leftHeight, rightHeight) +1;
+    return ans;
+
+
+}
+int main(){
+    Node* root;
+    root = buildTree();  
+    lOt(root);
+    cout << "Height " << height(root) << endl;
+     
+    return 0;
+
+}
