31f709fb1a397e6dab9abd8f116bde5976283e95
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-26 19:33:00 +0530

    Add files via upload

---
+++ maxheap.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int getMaxHeap(int arr[] , int n , int  k){
+    priority_queue <int> pq;
+    //insert initial k elements
+    for(int i =0;i<k;i++){
+        pq.push(arr[i]);
+
+
+    }
+    for(int i =k;i<n;i++){
+        int element = arr[i];
+        if(element < pq.top()){
+            pq.pop();
+            pq.push(element);
+
+        }
+    }
+    int ans = pq.top();
+    return ans;
+
+}
+int main(){
+    int arr[] = {10,5,50,20,4,15};
+    int n = 6;
+    int k = 6;
+    int ans = getMaxHeap(arr, n , k);
+  cout << ans;
+  return 0;
+
+    }
